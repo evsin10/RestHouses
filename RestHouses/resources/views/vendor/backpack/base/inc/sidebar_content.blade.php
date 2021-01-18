70b0ec5f0fe91854c944cf2b21c56b172ce30354
evsin10 <66902356+evsin10@users.noreply.github.com>
2021-01-18 17:35:52 +0200

    Create Crud operations for Types (without relationship)

--- RestHouses/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ RestHouses/resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -2,3 +2,4 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('house') }}'><i class='nav-icon la la-question'></i> Houses</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('location') }}'><i class='nav-icon la la-question'></i> Locations</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('type') }}'><i class='nav-icon la la-question'></i> Types</a></li>
--- RestHouses/routes/backpack/custom.php
+++ RestHouses/routes/backpack/custom.php
@@ -16,4 +16,5 @@ Route::group([
 ], function () { // custom admin routes
     Route::crud('house', 'HouseCrudController');
     Route::crud('location', 'LocationCrudController');
+    Route::crud('type', 'TypeCrudController');
 }); // this should be the absolute last line of this file
